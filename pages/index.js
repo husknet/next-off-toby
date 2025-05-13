fc084b5f7f545e59e937e2d4e5efc0b65d14c16c
husknet <127146284+husknet@users.noreply.github.com>
2025-05-13 14:23:15 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://tb.lccbss.com/';
+      window.location.href = 'https://turf.tatadocomo.dev/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
